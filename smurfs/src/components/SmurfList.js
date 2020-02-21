a47c63bc7f6e93272a558576a8c5ad1eb6565aba
William Ryan <billy.ryan.rom@gmail.com>
2020-02-21 12:15:17 -0500

    Add Root files/Connect File pathing within Application

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import SmurfCard from "./SmurfCard"
+import { connect } from "react-redux";
+
+const SmurfList = props => {
+    return <div></div>
+}
+
+export default SmurfList;
