d3b5f2d7e6f6fda66809488c5d338e6d5be6d2eb
William Ryan <billy.ryan.rom@gmail.com>
2020-02-21 12:33:31 -0500

    Add Connect, useEffect Hooks to SmurfList.js/Add Array Mapping to smurfs then pass it down to SmurfCard.js

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -2,8 +2,35 @@ import React, { useEffect } from "react";
 import SmurfCard from "./SmurfCard"
 import { connect } from "react-redux";
 
+import { fetchSmurfs } from "../actions";
+
 const SmurfList = props => {
-    return <div></div>
+
+    useEffect(() => {
+        props.fetchSmurfs()
+    }, [])
+
+    return (
+        <div>
+            <h1 className="title">Smurf Village App</h1>
+            <div className = "card-container">
+                {props.smurfs.map(smurf => (
+                    <SmurfCard smurf={smurf}/>
+                ))}
+            </div>
+        </div>
+    )
 }
 
-export default SmurfList;
+const mapStateToProps = state => {
+    return {
+        smurfs: state.smurfs,
+        isFetching: state.isFetching,
+        errors: state.errors
+    }
+}
+
+export default connect(
+    mapStateToProps, 
+    { fetchSmurfs }
+)(SmurfList);
